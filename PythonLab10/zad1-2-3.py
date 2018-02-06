b083fd6d14b838e9355022d3a5b956492a39f785
Kamil Maksim <kamil.maxim@gmail.com>
2018-02-06 19:43:34 +0100

    Update zad1-2-3.py

--- PythonLab10/zad1-2-3.py
+++ PythonLab10/zad1-2-3.py
@@ -2,30 +2,32 @@ import sqlite3
 
 conon = sqlite3.connect('test.db')
 
-conon.execute('''CREATE TABLE IF NOT EXISTS
-BOOKS(ID INT PRIMARY KEY     NOT NULL,
-       TITLE           TEXT    NOT NULL,
-       AUTHOR          TEXT    NOT NULL,
-       YEAR            INTEGER NOT NULL,
-       ISBN            TEXT    NOT NULL);''')
+conon.execute('''CREATE TABLE IF NOT EXISTS BOOKS
+        (ID INT PRIMARY KEY     NOT NULL,
+        TITLE           TEXT    NOT NULL,
+        AUTHOR          TEXT    NOT NULL,
+        YEAR            INTEGER NOT NULL,
+        ISBN            TEXT    NOT NULL)''')
 
-conon.execute('''CREATE TABLE IF NOT EXISTS
-READERS(ID INT PRIMARY KEY    NOT NULL,
-       FIRSTNAME             TEXT    NOT NULL,
-       LASTNAME              TEXT    NOT NULL,
-       BOOK      FOREIGN KEY INT     NOT NULL,
-       RETURNDATE            DATE    NOT NULL);''')
+conon.execute('''CREATE TABLE IF NOT EXISTS READERS
+        (ID     INT     PRIMARY KEY    NOT NULL,
+        FIRSTNAME             TEXT    NOT NULL,
+        LASTNAME              TEXT    NOT NULL)''')
+
+conon.execute('''CREATE TABLE IF NOT EXISTS BOOKS_READERS
+        (BOOK_ID     INT,
+        READER_ID   INT,
+        FOREIGN KEY(BOOK_ID)    REFERENCES  BOOKS(ID),
+        FOREIGN KEY(READER_ID)  REFERENCES  READERS(ID))''')
 
 
-print "Utworzono tabele";
+print "Utworzono tabele"
 
 try:
-    conon.execute("INSERT INTO BOOKS (TITLE, AUTHOR, YEAR, ISBN) \
-          VALUES (1, 'Zew Krwi', 'Jack London', 1966, 'ABC-654' )");
-    conon.execute("INSERT INTO BOOKS (TITLE, AUTHOR, YEAR, ISBN) \
-          VALUES (2, 'Bialy Kiel', 'Jack London', 1972, 'ABD-134' )");
-    conon.execute("INSERT INTO READERS (FIRSTNAME, LASTNAME, BOOK, RETURNDATE) \
-          VALUES ('Janusz', 'Kowalski', '1', '2018-10-10' )");
+    conon.execute("INSERT INTO BOOKS VALUES (1, 'Zew Krwi', 'Jack London', 1966, 'ABC-654' )");
+    conon.execute("INSERT INTO BOOKS VALUES (2, 'Bialy Kiel', 'Jack London', 1972, 'ABD-134' )");
+    conon.execute("INSERT INTO READERS VALUES (1, 'Janusz', 'Kowalski')");
+    conon.execute("INSERT INTO BOOKS_READERS VALUES (2, 1)");
     conon.commit()
 
     cursor = conon.execute('''SELECT * FROM BOOKS''')
