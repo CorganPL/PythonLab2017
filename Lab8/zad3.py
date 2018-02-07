7951edc3c8a505d4106f92ba06ad99565ad87c70
Kamil Maksim <kamil.maxim@gmail.com>
2018-02-07 12:47:57 +0100

    Update zad3.py

--- Lab8/zad3.py
+++ Lab8/zad3.py
@@ -1,49 +1,68 @@
 from Tkinter import *
 
-def ref(x):
-    global operation
-    operation += x
+class Calc(Frame):
 
-master = Tk()
+    def __init__(self):
+        Frame.__init__(self)
 
-operation = ""
-operationLabel = Label(master, text="")
-operationLabel.grid(row=0)
+        self.string = ""
+        self.pack(expand=YES, fill=BOTH)
+        self.master.title("Tytul")
 
-resultLabel = Label(master, text="")
-resultLabel.grid(row=0)
+        self.button1 = Button(self, text="1", fg="black", command=lambda: self.przycisk("1"), width=16, height=1)
+        self.button2 = Button(self, text="2", fg="black", command=lambda: self.przycisk("2"), width=16, height=1)
+        self.button3 = Button(self, text="3", fg="black", command=lambda: self.przycisk("3"), width=16, height=1)
+        self.button4 = Button(self, text="4", fg="black", command=lambda: self.przycisk("4"), width=16, height=1)
+        self.button5 = Button(self, text="5", fg="black", command=lambda: self.przycisk("5"), width=16, height=1)
+        self.button6 = Button(self, text="6", fg="black", command=lambda: self.przycisk("6"), width=16, height=1)
+        self.button7 = Button(self, text="7", fg="black", command=lambda: self.przycisk("7"), width=16, height=1)
+        self.button8 = Button(self, text="8", fg="black", command=lambda: self.przycisk("8"), width=16, height=1)
+        self.button9 = Button(self, text="9", fg="black", command=lambda: self.przycisk("9"), width=16, height=1)
+        self.button0 = Button(self, text="0", fg="black", command=lambda: self.przycisk("0"), width=16, height=1)
+        self.buttonplus = Button(self, text="+", fg="black", command=lambda: self.przycisk("+"), width=16, height=1)
+        self.buttonminus = Button(self, text="-", fg="black", command=lambda: self.przycisk("-"), width=16, height=1)
+        self.buttonmnoz = Button(self, text="*", fg="black", command=lambda: self.przycisk("*"), width=16, height=1)
+        self.buttondziel = Button(self, text="/", fg="black", command=lambda: self.przycisk("/"), width=16, height=1)
+        self.buttonwynik = Button(self, text="=", fg="black", command=self.oblicz, width=16, height=1)
+
+        self.Podglad = Label(self, text=self.string)
+        self.Label = Label(self, text=self.string)
+
+        self.button1.grid(row=0, column=0)
+        self.button2.grid(row=0, column=1)
+        self.button3.grid(row=0, column=2)
+        self.button4.grid(row=1, column=0)
+        self.button5.grid(row=1, column=1)
+        self.button6.grid(row=1, column=2)
+        self.button7.grid(row=2, column=0)
+        self.button8.grid(row=2, column=1)
+        self.button9.grid(row=2, column=2)
+        self.button0.grid(row=2, column=3)
+        self.buttonplus.grid(row=0, column=3)
+        self.buttonminus.grid(row=1, column=3)
+        self.buttonmnoz.grid(row=3, column=1)
+        self.buttondziel.grid(row=3, column=0)
+        self.buttonwynik.grid(row=3, column=2)
+
+        self.Label.grid(row=4, column=0)
+
+    def przycisk(self, arg):
+        self.string += arg
+        self.Podglad.pack_forget()
+
+        self.Podglad = Label(self, text="Dzialanie: " + self.string)
+        self.Podglad.grid(row=4, column=0)
+
+    def oblicz(self):
+        wynik = eval(self.string)
+        self.Label.pack_forget()
+        self.Label = Label(self, text="Wynik: " + str(wynik))
+        self.Label.grid(row=5, column=0)
 
 
-b1 = Button(master, text="1", command = lambda: ref(1))
-b1.grid(row=0, column=0)
-b2 = Button(master, text="2", command = lambda: ref(2))
-b2.grid(row=0, column=1)
-b3 = Button(master, text="3", command = lambda: ref(3))
-b3.grid(row=0, column=2)
-b4 = Button(master, text="4", command = lambda: ref(4))
-b4.grid(row=1, column=0)
-b5 = Button(master, text="5", command = lambda: ref(5))
-b5.grid(row=1, column=1)
-b6 = Button(master, text="6", command = lambda: ref(6))
-b1.grid(row=1, column=2)
-b7 = Button(master, text="7", command = lambda: ref(7))
-b1.grid(row=2, column=0)
-b8 = Button(master, text="8", command = lambda: ref(8))
-b1.grid(row=2, column=1)
-b9 = Button(master, text="9", command = lambda: ref(9))
-b1.grid(row=2, column=2)
-b0 = Button(master, text="0", command = lambda: ref(0))
-b1.grid(row=3, column=1)
+def main():
+    Calc().mainloop()
 
-badd = Button(master, text="+", command = lambda: ref("+"))
-badd.grid(row=4, column=0)
-bsub = Button(master, text="-", command = lambda: ref(1))
-bsub.grid(row=4, column=1)
-bmul = Button(master, text="x", command = lambda: countdown(seconds))
-bmul.grid(row=4, column=2)
-bdiv = Button(master, text="/", command = lambda: countdown(seconds))
-bdiv.grid(row=5, column=1)
-bequal = Button(master, text="=", command = lambda: countdown(seconds))
-bequal.grid(row=5, column=2)
+if __name__ == "__main__":
+    main()
 
-master.mainloop()
