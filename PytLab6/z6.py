aa521516789106a4402a660f30bdf61ca71cfcbf
Kamil Maksim <kamil.maxim@gmail.com>
2018-02-07 13:09:57 +0100

    Update z6.py

--- PytLab6/z6.py
+++ PytLab6/z6.py
@@ -1,8 +1,18 @@
 import urllib2
 from xml.dom import minidom
 
-response = urllib2.urlopen("https://www.metal-archives.com/bands/Helloween/159")
-html = response.read()
-doc = minidom.parseString(html)
+def zapisDoPliku(url):
+    with open("band.xml", 'w') as sitefile:
+        sitefile.write(urllib2.urlopen(url).read())
 
-print html
+zapisDoPliku('https://www.metal-archives.com/bands/Manowar/83')
+xmldoc = minidom.parse('band.xml')
+name = xmldoc.getElementsByTagName('name')
+year = xmldoc.getElementsByTagName('formed')
+genre = xmldoc.getElementsByTagName('genre')
+location = xmldoc.getElementsByTagName('location')
+
+print "name: " + name.childNodes[0].nodeValue
+print "year: " + year.childNodes[0].nodeValue
+print "genre: " + genre.childNodes[0].nodeValue
+print "location: " + location.childNodes[0].nodeValue
